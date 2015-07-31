01a6dfb72bee935584f7e87951edd426b9cf2c7c
Ondrej Sevcik <ondrej.sev@gmail.com>
2015-07-31 14:13:29 -0400

    Add comments for Status enum values

--- KInspector.Core/Status.cs
+++ KInspector.Core/Status.cs
@@ -4,10 +4,28 @@ namespace KInspector.Core
     /// Result's status flag
     /// </summary>
     public enum Status
-    { 
+    {
+        /// <summary>
+        /// Used in cases when displaying general information. 
+        /// </summary>
         Info = 0,
+
+
+        /// <summary>
+        /// Indicates issues without any problems.
+        /// </summary>
         Good = 1,
+
+
+        /// <summary>
+        /// Indicates issue that may be a problem.
+        /// </summary>
         Warning = 2,
+
+
+        /// <summary>
+        /// Indicates error that needs to be fixed.
+        /// </summary>
         Error = 3
     }
 }
