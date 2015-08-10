3b921e7093a1304603defea8d56423ae22b82e1b
Ondrej Sevcik <ondrej.sev@gmail.com>
2015-08-10 11:25:24 -0400

    Add 'Page type is assigned to a site' module #23

---
+++ KInspector.Modules/Scripts/PageTypeAssignedToSiteModule.sql
@@ -0,0 +1,4 @@
+﻿SELECT distinct SiteName, ClassName, NodeSiteID, NodeClassID
+FROM VIEW_CMS_Tree_Joined 
+LEFT JOIN CMS_ClassSite ON SiteID = NodeSiteID AND ClassID = NodeClassID
+WHERE SiteID IS NULL OR ClassID IS NULL
