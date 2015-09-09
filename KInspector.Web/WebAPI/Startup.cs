9cf0c014dd94eefe7a5b0615cc0837e00e716759
Petr Švihlík <svihlik.petr@gmail.com>
2015-09-09 21:51:04 +0200

    #42 - formatting

--- KInspector.Web/WebAPI/Startup.cs
+++ KInspector.Web/WebAPI/Startup.cs
@@ -4,24 +4,24 @@ using Owin;
 
 namespace KInspector.Web.WebAPI
 {
-    /// <summary>
-    /// Configures WebAPI (Self-host).
-    /// </summary>
-    public class Startup
-    {
-        public void Configuration(IAppBuilder appBuilder)
-        {
-            // Cors is needed to request localhost server from filesystem
-            appBuilder.UseCors(CorsOptions.AllowAll);
+	/// <summary>
+	/// Configures WebAPI (Self-host).
+	/// </summary>
+	public class Startup
+	{
+		public void Configuration(IAppBuilder appBuilder)
+		{
+			// Cors is needed to request localhost server from filesystem
+			appBuilder.UseCors(CorsOptions.AllowAll);
 
-            HttpConfiguration config = new HttpConfiguration();
-            config.Routes.MapHttpRoute(
-                name: "DefaultApi",
-                routeTemplate: "api/{controller}/{action}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
-	        appBuilder.UseStaticFiles("/FrontEnd");
-            appBuilder.UseWebApi(config);
-        }
-    }
+			HttpConfiguration config = new HttpConfiguration();
+			config.Routes.MapHttpRoute(
+				name: "DefaultApi",
+				routeTemplate: "api/{controller}/{action}/{id}",
+				defaults: new { id = RouteParameter.Optional }
+			);
+			appBuilder.UseStaticFiles("/FrontEnd");
+			appBuilder.UseWebApi(config);
+		}
+	}
 }
