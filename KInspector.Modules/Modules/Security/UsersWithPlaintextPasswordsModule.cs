66c0b9f694bd4a416bf2cb0b3b52fc172e372df5
Dan Walker <dan@rowdypixel.com>
2015-10-06 15:01:46 -0400

    Add documentation about why passwords should not be stored in plain text.

--- KInspector.Modules/Modules/Security/UsersWithPlaintextPasswordsModule.cs
+++ KInspector.Modules/Modules/Security/UsersWithPlaintextPasswordsModule.cs
@@ -10,15 +10,22 @@ namespace KInspector.Modules.Modules.Security
             return new ModuleMetadata
             {
                 Name = "Users with plaintext passwords",
-                Comment = "Selects all users with plain text passwords",
-                SupportedVersions = new[] { 
+                SupportedVersions = new[] {
                     new Version("6.0"),
                     new Version("7.0"),
-                    new Version("8.0"), 
-                    new Version("8.1"), 
-                    new Version("8.2") 
+                    new Version("8.0"),
+                    new Version("8.1"),
+                    new Version("8.2")
                 },
                 Category = "Security",
+                Comment = @"Displays a list of all users with passwords that are stored in plain text.
+
+Passwords stored in plain text greatly increase the risk to users in the event that unauthorized users gain access your database and increase the risk that your site may be accessed through a brute force attack.
+By ensuring that all stored password are salted and hashed, you can significantly mitigate the damage done in the event of a database breach, and decrease the risk of unauthorized access to sensitive parts of your site.
+
+For more information, see the documentation:
+https://docs.kentico.com/display/K82/Password+encryption+in+database"
+
             };
         }
 
