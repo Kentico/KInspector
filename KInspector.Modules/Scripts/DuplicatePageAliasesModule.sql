adb14d2646c214f22e8e62f9fb31d8a6073e6368
Mike Foden <mike.foden@twiztedblue.com>
2015-12-29 18:30:40 +1100

    'All Cultures' fix.
    Added in a fix to account for when an alias is set up for 'All
    Cultures'.

--- KInspector.Modules/Scripts/DuplicatePageAliasesModule.sql
+++ KInspector.Modules/Scripts/DuplicatePageAliasesModule.sql
@@ -1,16 +1,21 @@
-﻿SELECT x.AliasURLPath, x.AliasCulture, x.Repetitions, x.AffectedNodeIDs, x.Reason
+﻿SELECT DISTINCT x.AliasURLPath, x.Repetitions, x.AffectedNodeIDs as 'Affected Node IDs', x.Reason
 FROM
 (
-	SELECT DISTINCT AliasURLPath, AliasCulture, Repetitions, CAST(AliasNodeID AS VARCHAR(20)) AS AffectedNodeIDs, Reason
+	SELECT DISTINCT AliasURLPath, Repetitions, CAST(AliasNodeID AS VARCHAR(20)) AS AffectedNodeIDs, Reason
 	FROM (
-		SELECT AliasURLPath, AliasCulture, AliasNodeID, COUNT(*) as Repetitions, 'AliasURLPath has been assigned multiple times to the same NodeID.' as Reason 
-		FROM CMS_DocumentAlias
-		GROUP BY AliasURLPath, AliasCulture, AliasNodeID
-	) a1
+			SELECT a1.AliasURLPath, a1.AliasNodeID, COUNT(*) as Repetitions, 'AliasURLPath has been assigned multiple times to the same NodeID.' as Reason 
+		FROM CMS_DocumentAlias a1
+		INNER JOIN CMS_DocumentAlias a2
+		ON a1.AliasURLPath = a2.AliasURLPath
+		AND a1.AliasNodeID = a2.AliasNodeID
+		AND (a1.AliasCulture = a2.AliasCulture OR a1.AliasCulture = '' OR a2.AliasCulture = '')
+		AND (a1.AliasID <> a2.AliasID)
+		GROUP BY a1.AliasURLPath, a1.AliasNodeID
+	) a3
 	WHERE Repetitions > 1
 
 	UNION
-	SELECT DISTINCT b2.AliasURLPath, b2.AliasCulture, b2.Repetitions, b6.UsedNodes AS AffectedNodeIDs, b2.Reason
+	SELECT DISTINCT b2.AliasURLPath, b2.Repetitions, b6.UsedNodes AS AffectedNodeIDs, b2.Reason
 	FROM (
 		SELECT AliasURLPath, AliasCulture, COUNT(*) as Repetitions, 'AliasURLPath has been assigned multiple times to different NodeIDs.' as Reason 
 		FROM 
@@ -24,25 +29,26 @@ FROM
 				Select ', ' + CAST(b4.AliasNodeID AS VARCHAR(20)) AS [text()]
 				From dbo.CMS_DocumentAlias b4
 				Where b4.AliasURLPath = b3.AliasURLPath
-				AND b4.AliasCulture = b3.AliasCulture
+				AND (b4.AliasCulture = b3.AliasCulture OR b4.AliasCulture = '' OR b3.AliasCulture = '')
+				AND (b4.AliasID <> b3.AliasID)
 				ORDER BY b4.AliasNodeID
 				For XML PATH ('')
 			), 3, 1000) UsedNodes
 	From dbo.CMS_DocumentAlias b3
 	) b6
 	ON b2.AliasURLPath = b6.AliasURLPath
-	AND b2.AliasCulture = b6.AliasCulture
+	AND (b2.AliasCulture = b6.AliasCulture OR b2.AliasCulture = '' OR b6.AliasCulture = '')
 	WHERE Repetitions > 1
 	
 	UNION
-	SELECT DISTINCT AliasURLPath, AliasCulture, Repetitions, CAST(AliasNodeID AS VARCHAR(20)) AS AffectedNodeIDs, Reason
+	SELECT DISTINCT AliasURLPath, Repetitions, CAST(AliasNodeID AS VARCHAR(20)) AS AffectedNodeIDs, Reason
 	FROM (
-		SELECT AliasURLPath, AliasCulture, AliasNodeID, COUNT(*) as Repetitions, 'AliasURLPath is used as a NodeAliasPath for a different NodeID.' as Reason 
+		SELECT AliasURLPath, AliasNodeID, COUNT(*) as Repetitions, 'AliasURLPath is used as a NodeAliasPath for NodeID ' + CAST(t.NodeID AS VARCHAR(20)) + '.' as Reason 
 		FROM CMS_DocumentAlias d
 		INNER JOIN CMS_Tree t
 		ON t.NodeAliasPath = d.AliasURLPath
 		AND t.NodeID <> d.AliasNodeID
-		GROUP BY AliasURLPath, AliasCulture, AliasNodeID
+		GROUP BY d.AliasURLPath, d.AliasNodeID, t.NodeID
 	) c1
 ) x
 ORDER BY AliasURLPath
