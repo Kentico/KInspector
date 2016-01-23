750b51b6eb758e5800a70fa1669a6e848eacef83
Anibal Velarde <anibalvelarde@gmail.com>
2016-01-23 16:47:05 -0600

    Revised version for Fixing Issue #17
    In this set of changes attempts to:
    * Modifies PasswordPolicyModule
    * Modifies PaswordPolicy.sql script
    * Modifies PasswordPolicyModuleTests
    * make corrections to issues described in previous bug report (#17)
    * leverage suggestions from made earlier to access array elements by
    using "ColumnName" rather than element position in the array, [x]

--- KInspector.Modules/Modules/Security/PasswordPolicyModule.cs
+++ KInspector.Modules/Modules/Security/PasswordPolicyModule.cs
@@ -3,6 +3,7 @@ using System.Configuration;
 using System.Data;
 using System.IO;
 using System.Web;
+using System.Linq;
 using System.Web.Configuration;
 using Kentico.KInspector.Core;
 
@@ -16,7 +17,6 @@ namespace Kentico.KInspector.Modules
             {
                 Name = "Password policy settings",
                 Comment = @"It is critical that passwords are stored securely in the database, the module checks that the default and recommended SHA2 option is configured. https://docs.kentico.com/display/K9/Password+encryption+in+database
-
 This module also checks that there is a password policy enforced to ensure users use a password that meets a minimum set of requirements. https://docs.kentico.com/display/K9/Password+strength+policy+and+its+enforcement",
                 SupportedVersions = new[] {
                     new Version("7.0"),
@@ -36,29 +36,29 @@ This module also checks that there is a password policy enforced to ensure users
             var results = dbService.ExecuteAndGetTableFromFile("PasswordPolicy.sql");
 
             if (results.Rows.Count > 0)
-            { 
-                DataRow[] passwordFormatRow = results.Select("KeyName = 'CMSPasswordFormat'");
-                DataRow[] passwordPolicyRow = results.Select("KeyName = 'CMSUsePasswordPolicy'");
+            {
+                DataRow[] passwordFormatRows = results.Select("KeyName = 'CMSPasswordFormat'");
+                DataRow[] passwordPolicyRows = results.Select("KeyName = 'CMSUsePasswordPolicy'");
 
-                if (passwordFormatRow[0][1].ToString() != "SHA2SALT")
-                {
-                    return new ModuleResults
-                    {
-                        Result = results,
-                        ResultComment = "The CMSPasswordFormat should be set to 'SHA2SALT'.",
-                        Status = Status.Error,
-                    };
-                }
-                else if (passwordPolicyRow[0][1].ToString() != "True")
-                {
-                    return new ModuleResults
-                    {
-                        Result = results,
-                        ResultComment = "It is recommended that you have CMSUsePasswordPolicy set to 'True'.",
-                        Status = Status.Warning,
-                    };
-                }
-                else
+                if (passwordFormatRows.Any(r => r["KeyValue"].ToString() != "SHA2SALT"))
+                        {
+                            return new ModuleResults
+                            {
+                                Result = results,
+                                ResultComment = "The CMSPasswordFormat should be set to 'SHA2SALT'.",
+                                Status = Status.Error,
+                            };
+                        } 
+                else if(passwordPolicyRows.Any(r => r["KeyValue"].ToString() != "True"))
+                            {
+                                return new ModuleResults
+                                {
+                                    Result = results,
+                                    ResultComment = "It is recommended that you have CMSUsePasswordPolicy set to 'True'.",
+                                    Status = Status.Warning,
+                                };
+                            }
+                else 
                 {
                     return new ModuleResults
                     {
