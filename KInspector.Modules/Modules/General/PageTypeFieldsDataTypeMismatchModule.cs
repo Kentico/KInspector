2d0c98913ce0256a4cd7ac2e48a87482fa2461f9
Ondrej Sevcik <ondrej.sev@gmail.com>
2016-05-27 10:04:52 +0200

    Shrink description

--- KInspector.Modules/Modules/General/PageTypeFieldsDataTypeMismatchModule.cs
+++ KInspector.Modules/Modules/General/PageTypeFieldsDataTypeMismatchModule.cs
@@ -19,12 +19,8 @@ namespace Kentico.KInspector.Modules
                     new Version("9.0"),
                 },
                 Comment = @"You may face this error when exporting/importing a site or when working with web parts / widgets that list more than one-page type. 
-
 This error is caused by at least two different page types (for example A and B) having a field named in the same way (for example FieldName) but in each page type the field data is stored as a different data type (for example for A, it is 'Text' and for B it is 'GUID').
-
-The best practice, in this case, is to use the page type code name as the Field name prefix. For example:
-
-CustomPageTypeA_FieldName
+The best practice, in this case, is to use the page type code name as the Field name prefix. For example: CustomPageTypeA_FieldName
 
 For more information, see https://devnet.kentico.com/articles/conversion-failed-when-converting-from-a-character-string-to-uniqueidentifier",
             };
