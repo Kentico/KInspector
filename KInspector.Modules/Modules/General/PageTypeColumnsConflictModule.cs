65cc9255b6f5647f041e31bd27ba2ccee85b1976
Ondrej Sevcik <ondrej.sev@gmail.com>
2016-05-27 08:22:45 +0200

    Improve name, description and query

--- KInspector.Modules/Modules/General/PageTypeColumnsConflictModule.cs
+++ KInspector.Modules/Modules/General/PageTypeColumnsConflictModule.cs
@@ -9,14 +9,22 @@ namespace Kentico.KInspector.Modules
         {
             return new ModuleMetadata
             { 
-                Name = "TODO: rename // (possible) Page type columns conflict module",
-                SupportedVersions = new[] { 
+                Name = "Page type fields data type mismatch",
+                SupportedVersions = new[] {
                     new Version("8.0"), 
                     new Version("8.1"), 
                     new Version("8.2"),
                     new Version("9.0"),
                 },
-                Comment = @"TODO add description",
+                Comment = @"You may face this error when exporting/importing a site or when working with web parts / widgets that list more than one-page type. 
+
+This error is caused by at least two different page types (for example A and B) having a field named in the same way (for example FieldName) but in each page type the field data is stored as a different data type (for example for A, it is 'Text' and for B it is 'GUID').
+
+The best practice, in this case, is to use the page type code name as the Field name prefix. For example:
+
+CustomPageTypeA_FieldName
+
+For more information, see https://devnet.kentico.com/articles/conversion-failed-when-converting-from-a-character-string-to-uniqueidentifier",
             };
         }
 
