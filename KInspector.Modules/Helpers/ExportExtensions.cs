5219f10f741e19cba0f9537731a9266b6d938d4b
Josef Dvorak <josefd@kentico.com>
2016-07-14 13:09:59 +0200

    Remove obsolete DocX package and related code

--- KInspector.Modules/Helpers/ExportExtensions.cs
+++ KInspector.Modules/Helpers/ExportExtensions.cs
@@ -4,9 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-using Novacode;
 using NPOI.SS.UserModel;
-using NPOI.WP.UserModel;
 using NPOI.XWPF.UserModel;
 
 namespace Kentico.KInspector.Modules
@@ -262,34 +260,6 @@ namespace Kentico.KInspector.Modules
 
             return table;
         }
-
-        #region obsolete
-
-        public static void InsertDataTable(this DocX document, DataTable table)
-        {
-            if (table != null && table.Columns.Count != 0 && table.Rows.Count != 0)
-            {
-                var documentTable = document.InsertTable(table.Rows.Count + 1, table.Columns.Count);
-
-                // Insert headers
-                for (int headerIndex = 0; headerIndex < table.Columns.Count; headerIndex++)
-                {
-                    documentTable.Rows[0].Cells[headerIndex].Paragraphs[0].InsertText(Convert.ToString(table.Columns[headerIndex]),
-                        false, new Formatting { Bold = true });
-                }
-
-                // Insert row data
-                for (int row = 0; row < table.Rows.Count; row++)
-                {
-                    for (int column = 0; column < table.Columns.Count; column++)
-                    {
-                        documentTable.Rows[row + 1].Cells[column].Paragraphs[0].InsertText(Convert.ToString(table.Rows[row][column]));
-                    }
-                }
-            }
-        }
-
-        #endregion
 
         #endregion
     }
--- KInspector.Modules/Helpers/ExportHelper.cs
+++ KInspector.Modules/Helpers/ExportHelper.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 
 using Kentico.KInspector.Core;
 
-using Novacode;
 using NPOI.SS.UserModel;
 using NPOI.XSSF.UserModel;
 using NPOI.XWPF.UserModel;
@@ -226,74 +225,7 @@ namespace Kentico.KInspector.Modules
             // XWPFDocument.Write closes the stream. This is the only way to "re-open" it.
             return new MemoryStream(stream.ToArray());
         }
-
-        private static Stream GetExportStreamDocx2(IEnumerable<string> moduleNames, InstanceInfo instanceInfo)
-        {
-            // Create DocX
-            DocX doc = DocX.Load(@"Templates\KInspectorReportTemplate.docx");
-            if (doc == null)
-            {
-                throw new Exception("Could open find export template \"Templates\\KInspectorReportTemplate.docx\"");
-            }
-
-            // Process "macros"
-            Dictionary<string, string> macros = new Dictionary<string, string>
-                {
-                    {"SiteName", Convert.ToString(instanceInfo.Uri)},
-                    {"SiteVersion", Convert.ToString(instanceInfo.Version)},
-                    {"SiteDirectory", Convert.ToString(instanceInfo.Directory)}
-                };
-
-            foreach (var macro in macros)
-            {
-                doc.ReplaceText($"{{% {macro.Key} %}}", macro.Value);
-            }
-
-            foreach (string moduleName in moduleNames)
-            {
-                var result = ModuleLoader.GetModule(moduleName).GetResults(instanceInfo);
-                switch (result.ResultType)
-                {
-                    case ModuleResultsType.String:
-                        doc.InsertParagraph($"{moduleName}: {result.Result} ({result.ResultComment})");
-                        break;
-                    case ModuleResultsType.List:
-                        doc.InsertParagraphs($"{moduleName}: ({result.ResultComment})");
-                        List<string> resultList = result.Result as List<string>;
-                        if (resultList != null)
-                        {
-                            var tbl = doc.InsertTable(resultList.Count, 1);
-                            for (int row = 0; row < tbl.RowCount; row++)
-                            {
-                                tbl.Rows[row].Cells[0].Paragraphs[0].InsertText(resultList[row]);
-                            }
-                        }
-                        break;
-                    case ModuleResultsType.Table:
-                        doc.InsertParagraphs($"{moduleName}: ({result.ResultComment})");
-                        DataTable resultTable = result.Result as DataTable;
-                        doc.InsertDataTable(resultTable);
-                        break;
-                    case ModuleResultsType.ListOfTables:
-                        doc.InsertParagraphs($"{moduleName}: ({result.ResultComment})");
-                        DataSet data = result.Result as DataSet;
-                        foreach (DataTable tab in data.Tables)
-                        {
-                            doc.InsertDataTable(tab);
-                        }
-                        break;
-                    default:
-                        doc.InsertParagraphs($"{moduleName}: ({result.ResultComment})");
-                        continue;
-                }
-            }
-
-            // Save as stream
-            MemoryStream memoryStream = new MemoryStream();
-            doc.SaveAs(memoryStream);
-            return memoryStream;
-        }
-
+        
         #endregion
     }
 }
