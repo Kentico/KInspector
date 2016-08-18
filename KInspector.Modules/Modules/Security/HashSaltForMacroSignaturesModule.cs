6502ef933a2dfe61ba6a92ea82044be5ca541541
EvanZimmerman <ewz23941@gmail.com>
2016-08-17 21:09:17 -0400

    Fixes #19.  Create a module for detecting if the CMSHashStringSalt key was found in the appsettings.

---
+++ KInspector.Modules/Modules/Security/HashSaltForMacroSignaturesModule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Kentico.KInspector.Core;
+using System.Configuration;
+
+namespace Kentico.KInspector.Modules
+{
+    public class HashSaltForMacroSignaturesModule : IModule
+    {
+        public ModuleMetadata GetModuleMetadata()
+        {
+            return new ModuleMetadata
+            {
+                Category = "Security",
+                Name = "Hash Salt For Macro Signatures",
+                SupportedVersions = new[] {
+                    new Version("7.0"),
+                    new Version("8.0"),
+                    new Version("8.1"),
+                    new Version("8.2"),
+                    new Version("9.0")
+                },
+                Comment = @"Ensures that the 'CMSHashStringSalt' key is found within the web configuration file."
+            };
+        }
+
+        public ModuleResults GetResults(IInstanceInfo instanceInfo)
+        {
+           if(ConfigurationManager.AppSettings["CMSHashStringSalt"] != null)
+           {
+                return new ModuleResults
+                {
+                    Status = Status.Good,
+                    ResultComment = "CMSHashStringSalt key found!"
+                };
+           }
+           else
+           {
+                return new ModuleResults
+                {
+                    Status = Status.Error,
+                    ResultComment = "CMSHashStringSalt key not found!"
+                };
+           }
+        }
+    }
+}
