db5316e617c61d3514f47ca3265b7abacb7ef482
KenticoBSoltis <bryans@kentico.com>
2016-09-08 14:04:49 -0500

    Added funcitonality to reportTop 25 tables by data (size).

--- KInspector.Modules/Modules/General/BigTablesModule.cs
+++ KInspector.Modules/Modules/General/BigTablesModule.cs
@@ -1,24 +1,23 @@
-﻿using System;
-using Kentico.KInspector.Core;
+﻿using System;using Kentico.KInspector.Core;
 
-namespace Kentico.KInspector.Modules
-{
+namespace Kentico.KInspector.Modules{
     public class BigTablesModule : IModule
     {
         public ModuleMetadata GetModuleMetadata()
         {
             return new ModuleMetadata
-            { 
-                Name = "Top 25 tables by size",
-                SupportedVersions = new[] { 
-                    new Version("6.0"),
-                    new Version("7.0"),
-                    new Version("8.0"), 
-                    new Version("8.1"), 
-                    new Version("8.2"),
-                    new Version("9.0")
-                },
-                Comment = @"Displays top 25 biggest tables from the database.",
+            {
+                Name = "Top 25 tables by size",
+                SupportedVersions = new[]{
+new Version("6.0"),
+new Version("7.0"),
+new Version("8.0"),
+new Version("8.1"),
+new Version("8.2"),
+new Version("9.0")
+},
+                Comment = @"Displays top 25 biggest tables from the database.",
+                Category = "Database"
             };
         }
 
@@ -27,14 +26,15 @@ namespace Kentico.KInspector.Modules
         {
             var dbService = instanceInfo.DBService;
 
-            int databaseSizeInMB = dbService.ExecuteAndGetScalar<int>("SELECT SUM(reserved_page_count) * 8.0 / 1024 FROM sys.dm_db_partition_stats");
-            var results = dbService.ExecuteAndGetTableFromFile("BigTablesModule.sql");
+            int databaseSizeInMB = dbService.ExecuteAndGetScalar<int>("SELECT SUM(reserved_page_count) * 8.0 / 1024 FROM sys.dm_db_partition_stats");
+            var results = dbService.ExecuteAndGetDataSetFromFile("BigTablesModule.sql");
+            results.Tables[0].TableName = "Top 25 Tables By Rows";
+            results.Tables[1].TableName = "Top 25 Tables By Data";
 
             return new ModuleResults
             {
                 Result = results,
-                ResultComment = String.Format("The overall database size is {0} MB", databaseSizeInMB)
+                ResultComment = String.Format("The overall database size is {0} MB", databaseSizeInMB)
             };
         }
-    }
-}
+    }}
