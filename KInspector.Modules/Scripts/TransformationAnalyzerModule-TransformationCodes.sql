978d387707529d9cf390cc2aa0fbf773382a116a
Christopher Jennings <ChrisJ@Kentico.com>
2016-12-21 10:46:49 -0500

    Remove extra SQL that was separated to another script

--- KInspector.Modules/Scripts/TransformationAnalyzerModule-TransformationCodes.sql
+++ KInspector.Modules/Scripts/TransformationAnalyzerModule-TransformationCodes.sql
@@ -1,10 +1,4 @@
-﻿IF NOT EXISTS (SELECT * FROM sys.types WHERE is_table_type = 1 AND name ='KI_TransformationNames')
-    CREATE TYPE dbo.KI_TransformationNames AS TABLE(
-        TransformationName VARCHAR(MAX)
-    )
-
-
-SELECT TransformationID, TransformationName, TransformationCode 
+﻿SELECT TransformationID, TransformationName, TransformationCode 
 	FROM CMS_Transformation 
 	WHERE TransformationName IN (SELECT * FROM @ListOfNames)
 
