4f7c3eb35714e42d03602ae2342b791c029cd07c
Christopher Jennings <ChrisJ@Kentico.com>
2017-01-12 10:56:16 -0500

    Add initial code for task processing issues module.

---
+++ KInspector.Modules/Modules/General/TaskProcessingIssuesModule.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Data;
+using Kentico.KInspector.Core;
+
+namespace Kentico.KInspector.Modules
+{
+    public class TaskProcessingIssues : IModule
+    {
+        public ModuleMetadata GetModuleMetadata()
+        {
+            return new ModuleMetadata
+            {
+                Name = "Task processing issues",
+                SupportedVersions = new[] {
+                    new Version("6.0"),
+                    new Version("7.0"),
+                    new Version("8.0"),
+                    new Version("8.1"),
+                    new Version("8.2"),
+                    new Version("9.0")
+                },
+                Comment = @"Checks for possible issues with processing system, workflow, marketing automation, smart search, and web farm tasks.",
+                Category = "Database"
+            };
+        }
+
+
+        public ModuleResults GetResults(IInstanceInfo instanceInfo)
+        {
+            var dbService = instanceInfo.DBService;
+            var results = dbService.ExecuteAndGetTableFromFile("TaskProcessingIssueModule.sql");
+
+            var hasUnprocessedTasks = false;
+            if (results != null && results.Rows.Count > 0)
+            {
+                var row = results.Rows[0];
+
+                foreach (DataColumn column in results.Columns)
+                {
+                    var value = int.Parse(row[column].ToString());
+                    if (value > 0)
+                    {
+                        hasUnprocessedTasks = true;
+                        break;
+                    }
+                }
+            }
+
+            return new ModuleResults
+            {
+                Result = results,
+                Status = hasUnprocessedTasks ? Status.Warning : Status.Good,
+                ResultComment = hasUnprocessedTasks ? "There are unprocessed tasks that should be reviewed." : "All tasks have been processed."
+            };
+        }
+    }
+}
