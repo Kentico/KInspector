8f1213636a01bd19b13173287c666544ebdbe5b7
Ilesh Mistry <ilesh.m@mmtdigital.co.uk>
2017-01-18 13:25:13 +0000

    Include V10 into the Supported Version list for the Screenshotter module

--- KInspector.Modules/Modules/General/ScreenshotterModule.cs
+++ KInspector.Modules/Modules/General/ScreenshotterModule.cs
@@ -26,7 +26,8 @@ namespace Kentico.KInspector.Modules
                     new Version("8.0"), 
                     new Version("8.1"), 
                     new Version("8.2"),
-                    new Version("9.0")
+                    new Version("9.0"),
+                    new Version("10.0")
                 },
                 Comment = @"It takes a screenshot of all published pages on the website. 
 - All screenshots are saved into your desktop folder
