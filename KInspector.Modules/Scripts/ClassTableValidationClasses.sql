09448d383ad56146884e5758449a5da4c6af8310
Josef Dvorak <josefd@kentico.com>
2017-05-31 04:12:58 +1000

    Change SQL script for missing tables

--- KInspector.Modules/Scripts/ClassTableValidationClasses.sql
+++ KInspector.Modules/Scripts/ClassTableValidationClasses.sql
@@ -1,6 +1,7 @@
 ﻿-- Classes with missing database tables.
-SELECT ClassDisplayName, ClassName FROM CMS_Class
-WHERE
-	ClassIsDocumentType = 0 AND
-	ClassTableName NOT IN (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES)
-ORDER BY ClassDisplayName, ClassName
+SELECT ClassDisplayName, ClassName, ClassTableName FROM CMS_Class
+	WHERE
+		ClassIsDocumentType = 0 AND
+		ClassTableName NOT IN (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES)
+	ORDER BY
+		ClassDisplayName, ClassName, ClassTableName
