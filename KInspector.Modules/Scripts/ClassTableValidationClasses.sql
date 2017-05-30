773bf986c63d039d76981c79b6f7379b22411744
Josef Dvorak <josefd@kentico.com>
2017-05-31 04:02:26 +1000

    Add Class table validation module

---
+++ KInspector.Modules/Scripts/ClassTableValidationClasses.sql
@@ -0,0 +1,6 @@
+﻿-- Classes with missing database tables.
+SELECT ClassDisplayName, ClassName FROM CMS_Class
+WHERE
+	ClassIsDocumentType = 0 AND
+	ClassTableName NOT IN (SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES)
+ORDER BY ClassDisplayName, ClassName
