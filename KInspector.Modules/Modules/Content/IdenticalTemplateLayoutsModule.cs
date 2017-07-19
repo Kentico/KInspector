61284f7d6c6f1b3f4d1493232572ec1ead1670ac
Christopher Jennings <ChrisJ@Kentico.com>
2017-07-19 13:09:20 -0400

    Add new identical layouts module

---
+++ KInspector.Modules/Modules/Content/IdenticalTemplateLayoutsModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using Kentico.KInspector.Core;
+
+namespace Kentico.KInspector.Modules
+{
+    public class IdenticalTemplateLayoutsModule : IModule
+    {
+        public ModuleMetadata GetModuleMetadata()
+        {
+            return new ModuleMetadata
+            {
+                Name = "Templates with identical layouts",
+                SupportedVersions = new[] {
+                    new Version("10.0")
+                },
+                Comment = @"Returns the list of templates with identical custom layouts (whitespace sensitive).",
+            };
+        }
+
+
+        public ModuleResults GetResults(IInstanceInfo instanceInfo)
+        {
+            var dbService = instanceInfo.DBService;
+            var identicalTemplateLayouts = dbService.ExecuteAndGetTableFromFile("IdenticalTemplateLayoutsModule.sql");
+
+            return new ModuleResults
+            {
+                Result = identicalTemplateLayouts
+            };
+        }
+    }
+}
---
+++ KInspector.Modules/Scripts/IdenticalTemplateLayoutsModule.sql
@@ -0,0 +1,11 @@
+﻿SELECT
+  CodeNames = STUFF(
+    (SELECT ', ' + PageTemplateCodeName
+      FROM CMS_PageTemplate b 
+        WHERE b.PageTemplateLayout = a.PageTemplateLayout
+        FOR XML PATH('')), 1, 2, ''),
+  a.PageTemplateLayout
+  FROM CMS_PageTemplate a
+  WHERE a.PageTemplateLayout is not NULL AND a.PageTemplateLayoutID is NULL
+  GROUP BY a.PageTemplateLayout
+  HAVING COUNT(*) > 1
