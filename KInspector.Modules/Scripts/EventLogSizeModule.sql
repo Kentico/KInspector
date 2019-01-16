d7640492f5dc7af6dac2423b3d61f72f9c00c3a3
Josef Dvorak <JosefD@kentico.com>
2019-01-16 18:10:43 +1100

    Remove Event log size module

--- KInspector.Modules/Scripts/EventLogSizeModule.sql
+++
@@ -1,4 +0,0 @@
-﻿SELECT ISNULL(CMS_Site.SiteDisplayName,'Global settings') as SiteDisplayName, CMS_SettingsKey.KeyName, CMS_SettingsKey.KeyValue
-FROM CMS_SettingsKey LEFT OUTER JOIN
-CMS_Site ON CMS_SettingsKey.SiteID = CMS_Site.SiteID
-WHERE (CMS_SettingsKey.KeyName IN ('CMSLogSize'))
