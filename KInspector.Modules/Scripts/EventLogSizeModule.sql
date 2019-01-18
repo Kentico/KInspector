7ac4ded13d1df41292391ae80fa3c75ca2e80cd5
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-01-18 10:35:29 -0500

    Adds event log size and password format checks back in (#185)

---
+++ KInspector.Modules/Scripts/EventLogSizeModule.sql
@@ -0,0 +1,4 @@
+﻿SELECT ISNULL(CMS_Site.SiteDisplayName,'Global settings') as SiteDisplayName, CMS_SettingsKey.KeyName, CMS_SettingsKey.KeyValue
+FROM CMS_SettingsKey LEFT OUTER JOIN
+CMS_Site ON CMS_SettingsKey.SiteID = CMS_Site.SiteID
+WHERE (CMS_SettingsKey.KeyName IN ('CMSLogSize'))
