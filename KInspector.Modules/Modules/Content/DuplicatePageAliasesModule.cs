b1e4a2ad8cbff11b3fdca90d542b40d2cf12ad70
Yuriy Sountsov <YuriyS@kentico.com>
2019-02-20 16:43:54 -0500

    Extended to include CMS_Tree as source of aliases and make it overall easier to read

--- KInspector.Modules/Modules/Content/DuplicatePageAliasesModule.cs
+++ KInspector.Modules/Modules/Content/DuplicatePageAliasesModule.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
+
 using Kentico.KInspector.Core;
+using Kentico.KInspector.Modules.Helpers.StrongTypedInfos;
 
 namespace Kentico.KInspector.Modules
 {
@@ -10,19 +13,19 @@ namespace Kentico.KInspector.Modules
         public ModuleMetadata GetModuleMetadata()
         {
             return new ModuleMetadata
-            { 
+            {
                 Name = "Duplicate Page Aliases",
-                SupportedVersions = new[] { 
+                SupportedVersions = new[] {
                     new Version("6.0"),
                     new Version("7.0"),
-                    new Version("8.0"), 
-                    new Version("8.1"), 
+                    new Version("8.0"),
+                    new Version("8.1"),
                     new Version("8.2"),
                     new Version("9.0"),
                     new Version("10.0"),
                     new Version("11.0")
                 },
-                Comment = @"Checks that the CMS_DocumentAlias table does not contain any duplicates.",
+                Comment = @"Checks that the CMS_DocumentAlias and CMS_Tree tables do not contain any duplicates.",
                 Category = "Content"
             };
         }
@@ -30,15 +33,15 @@ namespace Kentico.KInspector.Modules
         public ModuleResults GetResults(IInstanceInfo instanceInfo)
         {
             var dbService = instanceInfo.DBService;
-            DataSet results = dbService.ExecuteAndGetDataSetFromFile("DuplicatePageAliasesModule.sql");
+            var results = instanceInfo.DBService.ExecuteAndGetDataSetFromFile("DuplicatePageAliasesModule.sql");
 
-            RemoveEmptyTables(results);
-
-            if (results.Tables.Count != 0)
+            if (results.Tables[0].Rows.Count > 0)
             {
+                var finalDataSet = BuildResultsBySite(results.Tables[0], results.Tables[1]);
+
                 return new ModuleResults
                 {
-                    Result = results,
+                    Result = finalDataSet,
                     ResultComment = "Document Alias issues found!",
                     Status = Status.Error,
                 };
@@ -51,15 +54,115 @@ namespace Kentico.KInspector.Modules
             };
         }
 
-
-        private void RemoveEmptyTables(DataSet dataSet)
+        private DataSet BuildResultsBySite(DataTable tableWithFirstColumnBeingSiteID, DataTable siteIDTable)
         {
-            var emptyTables = dataSet.Tables.Cast<DataTable>()
-                .Where(table => table.Rows.Count == 0).ToList();
+            var dataSet = new DataSet();
 
-            foreach (var emptyTable in emptyTables)
+            for (int i = 0; i < siteIDTable.Rows.Count; i++)
             {
-                dataSet.Tables.Remove(emptyTable);
+                var siteInfo = new SiteInfo(siteIDTable.Rows[i]);
+                var table = GetAttachmentsDataTable(siteInfo.SiteDisplayName);
+
+                foreach (DataRow row in tableWithFirstColumnBeingSiteID.Select($"{nameof(AliasInfo.AliasSiteID)} = {siteInfo.SiteID}"))
+                {
+                    table.Rows.Add(
+                        GetAliasRowWithParsedReason(new AliasInfo(row))
+                    );
+                }
+
+                dataSet.Tables.Add(table);
+            }
+
+            return dataSet;
+        }
+
+        private static DataTable GetAttachmentsDataTable(string tableName)
+        {
+            var attachmentsTable = new DataTable(tableName);
+            attachmentsTable.Columns.Add(AliasInfo.DisplayNames[nameof(AliasInfo.AliasURLPath)]);
+            attachmentsTable.Columns.Add(AliasInfo.DisplayNames[nameof(AliasInfo.AffectedNodeIDs)]);
+            attachmentsTable.Columns.Add(AliasInfo.DisplayNames[nameof(AliasInfo.OriginalNodeID)]);
+            attachmentsTable.Columns.Add(AliasInfo.DisplayNames[nameof(AliasInfo.Reasons)]);
+
+            return attachmentsTable;
+        }
+
+        private object[] GetAliasRowWithParsedReason(AliasInfo alias)
+        {
+            var nodeIDs = alias.AffectedNodeIDs
+                            .Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(i => int.Parse(i.ToString()));
+
+            int originalNodeID = alias.OriginalNodeID;
+
+            var reasons = string.Empty;
+
+            // No duplicate IDs means that the duplicates are all in CMS_DocumentAlias
+            if (!AnyDuplicates(nodeIDs))
+            {
+                reasons += "Same alias for different nodes in CMS_DocumentAlias. ";
+            }
+
+            // Original node ID means that one of the duplicates is in CMS_Tree
+            if (originalNodeID > 0)
+            {
+                reasons += "Duplicate alias in CMS_DocumentAlias as the original alias in CMS_Tree. ";
+            }
+
+            // Any duplicates exist which are not the original node ID
+            if (AnyDuplicates(nodeIDs.Where(i => !i.Equals(originalNodeID))))
+            {
+                reasons += "Duplicate aliases for the same node in CMS_DocumentAlias.";
+            }
+
+            return new object[] {
+                alias.AliasURLPath,
+                string.Join(", ", nodeIDs),
+                alias.OriginalNodeID,
+                reasons
+            };
+        }
+
+        private static bool AnyDuplicates(IEnumerable<int> nodeIDs)
+        {
+            return nodeIDs
+                .GroupBy(x => x)
+                .Where(g => g.Count() > 1)
+                .Any();
+        }
+
+        private class SiteInfo : SimpleBaseInfo
+        {
+            public int SiteID => Get<int>(nameof(SiteID));
+
+            public string SiteDisplayName => Get<string>(nameof(SiteDisplayName));
+
+            public SiteInfo(DataRow row) : base(row)
+            {
+            }
+        }
+
+        private class AliasInfo : SimpleBaseInfo
+        {
+            public int AliasSiteID => Get<int>(nameof(AliasSiteID));
+
+            public string AliasURLPath => Get<string>(nameof(AliasURLPath));
+
+            public string AffectedNodeIDs => Get<string>(nameof(AffectedNodeIDs));
+
+            public int OriginalNodeID => Get<int>(nameof(OriginalNodeID));
+
+            public string Reasons => "Reasons";
+
+            public static IDictionary<string, string> DisplayNames = new Dictionary<string, string>{
+                { nameof(AliasURLPath), "Alias URL path"},
+                { nameof(AffectedNodeIDs), "Affected node IDs"},
+                { nameof(OriginalNodeID), "Original node ID"},
+                { nameof(Reasons), "Reasons"}
+            };
+
+            public AliasInfo(DataRow row) : base(row)
+            {
             }
         }
     }
