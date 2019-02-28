923c772e4ab89eeb09aac315e4b395f31bd943a7
Yuriy Sountsov <34716163+yuriys-kentico@users.noreply.github.com>
2019-02-28 07:30:37 -0500

    Page not founds module revisions (#192)
    * Return DataSet method and SimpleBaseInfo support for other modules
    
    * Add Event Last Date column

---
+++ KInspector.Modules/Helpers/StrongTypedInfos/SimpleBaseInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+namespace Kentico.KInspector.Modules.Helpers.StrongTypedInfos
+{
+    public class SimpleBaseInfo
+    {
+        private readonly DataRow _row;
+
+        protected T Get<T>(string columnName)
+        {
+            if (_row[columnName] is DBNull)
+            {
+                return default(T);
+            }
+            return (T)_row[columnName];
+        }
+
+        protected SimpleBaseInfo(DataRow row)
+        {
+            _row = row;
+        }
+    }
+}
