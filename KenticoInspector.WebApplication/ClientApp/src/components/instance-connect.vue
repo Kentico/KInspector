d7476a1512c7005c013aa28ea5ddfbc5e1151217
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-04 19:36:19 -0500

    Add start of form

--- KenticoInspector.WebApplication/ClientApp/src/components/instance-connect.vue
+++ KenticoInspector.WebApplication/ClientApp/src/components/instance-connect.vue
@@ -8,9 +8,40 @@
         <h1>
           Connect to Kentico EMS instance
         </h1>
-        <p>
-          TBD
-        </p>
+        <v-form
+          ref="form"
+          v-model="valid"
+          lazy-validation
+          >
+          <v-text-field
+            v-model="instance.databaseConnection.server"
+            :rules="[v => !!v || 'Server is required']"
+            label="Server"
+            required
+            />
+
+          <v-text-field
+            v-model="instance.databaseConnection.database"
+            :rules="[v => !!v || 'Database is required']"
+            label="Database"
+            required
+            />
+
+          <v-btn
+            :disabled="!valid"
+            color="success"
+            @click="validate"
+            >
+            Validate
+          </v-btn>
+
+          <v-btn
+            color="error"
+            @click="reset"
+          >
+            Reset Form
+          </v-btn>
+        </v-form>
       </v-flex>
     </v-layout>
   </v-container>
@@ -19,6 +50,7 @@
 <script>
   export default {
     data: () => ({
+      valid: true,
       instance: {
         databaseConnection: {
           server: "",
@@ -27,7 +59,17 @@
         url: "",
         path: ""
       }
-    })
+    }),
+    methods: {
+      validate () {
+        if (this.$refs.form.validate()) {
+          this.snackbar = true
+        }
+      },
+      reset () {
+        this.$refs.form.reset()
+      }
+    }
   }
 </script>
 
