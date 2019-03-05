b6ff2d2c76312551cf6c4ab7755b14c9d7b0d002
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-05 17:18:23 -0500

    Add tabs to connect page

---
+++ KenticoInspector.WebApplication/ClientApp/src/components/card-coming-soon.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-card>
+    <v-card-text>
+      Coming Soon...
+    </v-card-text>
+  </v-card>
+</template>
--- KenticoInspector.WebApplication/ClientApp/src/views/instance-connect.vue
+++ KenticoInspector.WebApplication/ClientApp/src/views/instance-connect.vue
@@ -9,19 +9,54 @@
     </v-layout>
     <v-layout justify-center>
       <v-flex xs12>
-        <instance-connect-form-manual />
+        <v-tabs
+          centered
+          color="primary"
+          dark
+          v-model="tab"
+          >
+          <v-tabs-slider></v-tabs-slider>
+          <v-tab key="new">
+            New Connection
+          </v-tab>
+          <v-tab key="recent">
+            Recent Connections
+          </v-tab>
+          <v-tab key="iis">
+            IIS Connection
+          </v-tab>
+        </v-tabs>
+        <v-tabs-items
+          v-model="tab"
+          >
+          <v-tab-item key="new">
+            <instance-connect-form-manual />
+          </v-tab-item>
+          <v-tab-item key="recent">
+            <card-coming-soon />
+          </v-tab-item>
+          <v-tab-item key="iis">
+            <card-coming-soon />
+          </v-tab-item>
+        </v-tabs-items>
+
       </v-flex>
     </v-layout>
   </v-container>
 </template>
 
 <script>
+import CardComingSoon from '../components/card-coming-soon'
 import InstanceConnectFormManual from '../components/instance-connect-form-manual'
 
 export default {
   components: {
+    CardComingSoon,
     InstanceConnectFormManual
-  }
+  },
+  data: () => ({
+    tab: null
+  })
 }
 </script>
 
