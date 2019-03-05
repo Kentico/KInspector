82b9717b823eaa1835724af1a35ccac19ab43764
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-05 11:23:34 -0500

    Finish up connect to instance mock

--- KenticoInspector.WebApplication/ClientApp/src/views/instance-connect.vue
+++ KenticoInspector.WebApplication/ClientApp/src/views/instance-connect.vue
@@ -1,13 +1,26 @@
 <template>
-  <InstanceConnect />
+  <v-container>
+    <v-layout>
+      <v-flex xs12>
+        <h1 class="display-4 mb-3">
+          Connect <span class="grey--text display-3 font-weight-light">to a Kentico EMS Instance</span>
+        </h1>
+      </v-flex>
+    </v-layout>
+    <v-layout justify-center>
+      <v-flex xs12>
+        <instance-connect-form-manual />
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-import InstanceConnect from '../components/instance-connect'
+import InstanceConnectFormManual from '../components/instance-connect-form-manual'
 
 export default {
   components: {
-    InstanceConnect
+    InstanceConnectFormManual
   }
 }
 </script>
