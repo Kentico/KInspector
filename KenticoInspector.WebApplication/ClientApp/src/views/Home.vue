11ca0c1534e662d256aaf8a2f1d86ec17108cacb
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-05 20:16:05 -0500

    Add data modification notice

--- KenticoInspector.WebApplication/ClientApp/src/views/Home.vue
+++ KenticoInspector.WebApplication/ClientApp/src/views/Home.vue
@@ -5,9 +5,25 @@
         <h1 class="display-2">
           Welcome <span class="display-1 font-weight-light">to Kentico Inspector</span>
         </h1>
-        <p>
-          The open source tool for analyzing Kentico EMS instances.
-        </p>
+        <p>The open source tool for analyzing Kentico EMS instances.</p>
+
+        <v-card
+          color="info"
+          dark
+          class="mb-3"
+          >
+          <v-card-title class="pb-0">
+            <v-icon
+              left
+              >
+              info
+            </v-icon>
+            <span class="title">Data Modification Notice</span>
+          </v-card-title>
+          <v-card-text class="title font-weight-light">
+            The application does not modify data without clear warning. Regardless, it is not recommended to use run this in production.
+          </v-card-text>
+        </v-card>
       </v-flex>
     </v-layout>
     <v-layout>
@@ -17,6 +33,7 @@
           dark
           color="green"
           to="/connect"
+          class="mx-auto"
           >
           Get Started
         </v-btn>
