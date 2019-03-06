42b9a9c1daa4dc9cb2f432a5ae21806928ea245a
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-06 17:03:50 -0500

    Add support for all pre-existing module results outputs

---
+++ KenticoInspector.WebApplication/ClientApp/src/components/report-result-details.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <div
+      v-if="type === 'String'"
+      key="string-result"
+      v-html="data"
+      >
+    </div>
+    <div
+      v-else-if="type === 'StringList'"
+      key="string-list-result"
+      >
+      <span
+        v-for="(item, index) in data"
+        :key="`result-${index}`"
+        v-html="`${item}<br />`"
+        >
+      </span>
+    </div>
+    <div
+      v-else-if="type === 'Table'"
+      key="table-result"
+      >
+      <report-result-details-table
+        :name="data.name"
+        :rows="data.rows"
+        ></report-result-details-table>
+    </div>
+    <div
+      v-else-if="type === 'TableList'"
+      key="table-list-result"
+      >
+      <report-result-details-table
+        v-for="(item, index) in data"
+        :key="`result-${index}`"
+        :name="item.name"
+        :rows="item.rows"
+        ></report-result-details-table>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import ReportResultDetailsTable from "./report-result-details-table"
+export default {
+  components: {
+    ReportResultDetailsTable
+  },
+  props: {
+    type: {
+      type: String,
+      required: true
+    },
+    data: {
+      required: true
+    }
+  }
+}
+</script>
