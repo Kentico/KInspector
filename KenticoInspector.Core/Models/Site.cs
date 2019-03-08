e56a6851fda46b00753896970e8de9d172c85866
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-08 12:22:17 -0500

    Remove old project references; Add start of new Core project

---
+++ KenticoInspector.Core/Models/Site.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KenticoInspector.Core.Models
+{
+    public class Site
+    {
+        public int ID { get; set; }
+
+        public Guid GUID { get; set; }
+
+        public string Name { get; set; }
+
+        public string DomainName { get; set; }
+
+        public string PresentationUrl { get; set; }
+
+        public bool ContentOnly { get; set; }
+    }
+}
---
+++ KenticoInspector.Core/Services/IInstanceService.cs
@@ -0,0 +1,12 @@
+﻿using KenticoInspector.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenticoInspector.Core.Services
+{
+    interface IInstanceService
+    {
+        Instance ConnectToInstance(InstanceConfiguration instanceConfiguration);
+    }
+}
