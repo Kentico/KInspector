521c3b195d5c498cfe0b3057cb03037481e95edb
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-03-15 15:21:50 -0400

    Make new repositories inherit from IRepository; Make IService for easier DI

--- KenticoInspector.Core/Repositories/Interfaces/ISiteRepository.cs
+++ KenticoInspector.Core/Repositories/Interfaces/ISiteRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace KenticoInspector.Core.Repositories.Interfaces
 {
-    public interface ISiteRepository
+    public interface ISiteRepository : IRepository
     {
         Site GetSite(Instance instance, int siteId);
         List<Site> GetSites(Instance instance);
