fab31e6cb3a1a1501f6a38082716e713ddf890b9
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-05-07 11:54:40 -0400

    Add "Consistency" report tag

--- KenticoInspector.Core/Constants/ReportTags.cs
+++ KenticoInspector.Core/Constants/ReportTags.cs
@@ -9,5 +9,6 @@ namespace KenticoInspector.Core.Constants
         public const string Database = "Database";
         public const string EventLog = "Event Log";
         public const string Health = "Health";
+        public const string Consistency = "Consistency";
     }
 }
