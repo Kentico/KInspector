b85f6dd42a61a50e1bead333d17c61e336102388
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-05-09 10:05:47 -0400

    Re-org class (CodeMaid)

--- KenticoInspector.Core/Models/ReportResults.cs
+++ KenticoInspector.Core/Models/ReportResults.cs
@@ -4,14 +4,14 @@ namespace KenticoInspector.Core.Models
 {
     public class ReportResults
     {
-        public string Status { get; set; }
-        public string Summary { get; set; }
-        public string Type { get; set; }
-        public dynamic Data { get; set; }
-
         public ReportResults()
         {
             Data = new ExpandoObject();
         }
+
+        public dynamic Data { get; set; }
+        public string Status { get; set; }
+        public string Summary { get; set; }
+        public string Type { get; set; }
     }
 }
