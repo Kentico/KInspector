736d4abe9f7175ffafd135435045d3e346b8b167
aem914 <aem914.dev@gmail.com>
2019-05-23 14:41:13 -0400

    Initial commit for files created.

---
+++ KenticoInspector.Reports/UnusedPageTypeSummary/Report.cs
@@ -0,0 +1,35 @@
+﻿using KenticoInspector.Core;
+using KenticoInspector.Core.Constants;
+using KenticoInspector.Core.Helpers;
+using KenticoInspector.Core.Models;
+using KenticoInspector.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace KenticoInspector.Reports.UnusedPageTypeSummary
+{
+    class Report : IReport
+    {
+        public string Codename => "";
+
+        public IList<Version> CompatibleVersions => new List<Version>();
+
+        public IList<Version> IncompatibleVersions => new List<Version>();
+
+        public string LongDescription => "";
+
+        public string Name => "";
+
+        public string ShortDescription => "";
+
+        public IList<string> Tags => new List<string>();
+
+        public ReportResults GetResults(Guid InstanceGuid)
+        {
+            return new ReportResults();
+        }
+    }
+}
---
+++ KenticoInspector.Reports/UnusedPageTypeSummary/Scripts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenticoInspector.Reports.UnusedPageTypeSummary
+{
+    public static class Scripts
+    {
+        public const string BaseDirectory = "ReplaceMe/";
+        public const string GetTop25LargestTables = BaseDirectory + "SQLFile.sql";
+    }
+}
