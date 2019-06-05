44598c204c81c802e7123d05cdd073e2fb887b4b
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-06-05 16:13:42 -0400

    Update latest version history to filter out draft versions

--- KenticoInspector.Reports/ContentTreeConsistencyAnalysis/Scripts/GetLatestVersionHistoryIdForAllDocuments.sql
+++ KenticoInspector.Reports/ContentTreeConsistencyAnalysis/Scripts/GetLatestVersionHistoryIdForAllDocuments.sql
@@ -3,5 +3,6 @@
 	(
 		SELECT MAX(VersionHistoryID) OVER (PARTITION BY DocumentID) AS LatestVersionHistoryID
 			FROM CMS_VersionHistory
+			WHERE WasPublishedFrom IS NOT NULL
 	) AS LatestVersionHistoryIDs
 	GROUP BY LatestVersionHistoryID
