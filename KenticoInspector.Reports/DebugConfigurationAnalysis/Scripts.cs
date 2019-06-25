2e687a8f31d2f2acd9af9994dbe11e228b38360b
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-06-25 16:06:47 -0400

    Add script for database settings keys

---
+++ KenticoInspector.Reports/DebugConfigurationAnalysis/Scripts.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenticoInspector.Reports.DebugConfigurationAnalysis
+{
+    public static class Scripts
+    {
+        public const string BaseDirectory = "DebugConfigurationAnalysis/Scripts/";
+        public const string GetDebugSettingsValues = BaseDirectory + "GetDebugSettingsValues.sql";
+    }
+}
---
+++ KenticoInspector.Reports/DebugConfigurationAnalysis/Scripts/GetDebugSettingsValues.sql
@@ -0,0 +1,14 @@
+﻿SELECT 
+	KeyName, KeyDisplayName, KeyValue
+FROM
+	CMS_SettingsKey 
+WHERE
+	KeyName = 'CMSDisableDebug' OR
+	( 
+		KeyType = 'boolean' and -- Get only boolean-type settings 
+		KeyName like 'CMSDebug%' and -- Get only debug-related settings
+		KeyName not like '%all%' and -- Filters out settings to also debug in the admin
+		KeyName not like '%live%' and -- Filters out settings to display debug information on the live site
+		KeyName not like '%stack%' and -- Filters out settings to display stack information
+		KeyName != 'CMSDebugMacrosDetailed' -- Filters out setting for showing more details for macro debugging
+	)
