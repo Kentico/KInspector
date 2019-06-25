8daf24b1c95452ec4e74907870e3c940b317c57d
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-06-25 16:07:44 -0400

    Start of debug config analysis report

---
+++ KenticoInspector.Reports/DebugConfigurationAnalysis/Models/SettingsKey.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenticoInspector.Reports.DebugConfigurationAnalysis.Models
+{
+    public class SettingsKey
+    {
+        public string KeyName { get; set; }
+        public string KeyDisplayName { get; set; }
+        public bool KeyValue { get; set; }
+    }
+}
---
+++ KenticoInspector.Reports/DebugConfigurationAnalysis/Report.cs
@@ -0,0 +1,70 @@
+﻿using KenticoInspector.Core;
+using KenticoInspector.Core.Constants;
+using KenticoInspector.Core.Models;
+using KenticoInspector.Core.Services.Interfaces;
+using KenticoInspector.Reports.DebugConfigurationAnalysis.Models;
+using System;
+using System.Collections.Generic;
+
+namespace KenticoInspector.Reports.DebugConfigurationAnalysis
+{
+    public class Report : IReport
+    {
+        readonly IDatabaseService _databaseService;
+        readonly IInstanceService _instanceService;
+
+        public Report(IDatabaseService databaseService, IInstanceService instanceService)
+        {
+            _databaseService = databaseService;
+            _instanceService = instanceService;
+        }
+
+        public string Codename => "debug-configuration-analysis";
+
+        public IList<Version> CompatibleVersions => new List<Version> {
+            new Version("10.0"),
+            new Version("11.0"),
+            new Version("12.0"),
+        };
+
+        public IList<Version> IncompatibleVersions => new List<Version>();
+
+        public string LongDescription => @"";
+
+        public string Name => "Debug Configuration Analysis";
+
+        public string ShortDescription => "Shows status of debug settings in the database and web.config file.";
+
+        public IList<string> Tags => new List<string> {
+           ReportTags.Health
+        };
+
+        public ReportResults GetResults(Guid InstanceGuid)
+        {
+            var instance = _instanceService.GetInstance(InstanceGuid);
+            var instanceDetails = _instanceService.GetInstanceDetails(instance);
+            _databaseService.ConfigureForInstance(instance);
+
+            var databaseSettingsKeys = _databaseService.ExecuteSqlFromFile<SettingsKey>(Scripts.GetDebugSettingsValues);
+            var compilationDebugSetting = false;
+
+            return CompileResults(databaseSettingsKeys, compilationDebugSetting);
+        }
+
+        private ReportResults CompileResults(IEnumerable<SettingsKey> databaseSettingsKeys, bool compilationDebugSetting)
+        {
+
+            return new ReportResults()
+            {
+                Data = new TableResult<SettingsKey>()
+                {
+                    Name = "Settings Key Values",
+                    Rows = databaseSettingsKeys
+                },
+                Status = ReportResultsStatus.Information,
+                Summary = String.Empty,
+                Type = ReportResultsType.Table
+            };
+        }
+    }
+}
