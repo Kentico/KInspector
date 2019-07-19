cd8217b9d1560db887366229da3d35f153ceccab
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-19 14:06:22 -0400

    Arrange clean results

--- KenticoInspector.Reports.Tests/WebPartPerformanceAnalysisTests.cs
+++ KenticoInspector.Reports.Tests/WebPartPerformanceAnalysisTests.cs
@@ -1,8 +1,12 @@
-﻿using KenticoInspector.Core.Constants;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using KenticoInspector.Core.Constants;
 using KenticoInspector.Core.Models;
 using KenticoInspector.Core.Services.Interfaces;
 using KenticoInspector.Reports.Tests.Helpers;
 using KenticoInspector.Reports.WebPartPerformanceAnalysis;
+using KenticoInspector.Reports.WebPartPerformanceAnalysis.Models;
 using Moq;
 using NUnit.Framework;
 
@@ -23,13 +27,24 @@ namespace KenticoInspector.Reports.Tests
         public void Should_ReturnGoodStatus_When_NoWebPartsHaveUnspecifiedColumns()
         {
             // Arrange
-
+            ArrangeAllQueries();
 
             // Act
             var results = _mockReport.GetResults(_mockInstance.Guid);
 
             // Assert
             Assert.That(results.Status == ReportResultsStatus.Good, $"Expected status when no web parts have unspecified columns is 'Good' not '{results.Status}'.");
+        }
+
+        private void ArrangeAllQueries(List<PageTemplate> affectedTemplates = null)
+        {
+            affectedTemplates = affectedTemplates ?? new List<PageTemplate>();
+            _mockDatabaseService.SetupExecuteSqlFromFile(Scripts.GetAffectedTemplates, affectedTemplates);
+
+            var affectedTemplateIds = affectedTemplates.Select(x => x.PageTemplateID);
+            var affectedDocuments = new List<Document>();
+            _mockDatabaseService.SetupExecuteSqlFromFileWithListParameter(Scripts.GetDocumentsByPageTemplateIds, "IDs", affectedTemplateIds, affectedDocuments);
+
         }
     }
 }
