c77919ca14b5a58e057dcd75179d20204de172f5
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-22 19:08:19 -0400

    Create abstract report to simplify creating reports

---
+++ KenticoInspector.Core/AbstractReport.cs
@@ -0,0 +1,30 @@
+﻿using KenticoInspector.Core.Models;
+using System;
+using System.Collections.Generic;
+
+namespace KenticoInspector.Core
+{
+    public abstract class AbstractReport : IReport
+    {
+        public string Codename => GetCodename(this.GetType());
+
+        public static string GetCodename(Type reportType) {
+            return GetDirectParentNamespace(reportType);
+        }
+
+        public abstract IList<Version> CompatibleVersions { get; }
+
+        public virtual IList<Version> IncompatibleVersions => new List<Version>();
+
+        public abstract IList<string> Tags { get; }
+
+        public abstract ReportResults GetResults();
+
+        private static string GetDirectParentNamespace(Type reportType)
+        {
+            var fullNameSpace = reportType.Namespace;
+            var indexAfterLastPeriod = fullNameSpace.LastIndexOf('.') + 1;
+            return fullNameSpace.Substring(indexAfterLastPeriod, fullNameSpace.Length - indexAfterLastPeriod);
+        }
+    }
+}
