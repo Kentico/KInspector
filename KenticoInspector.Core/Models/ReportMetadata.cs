80ee0f98d93dab4a2649361bb9530a674e0fdb60
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-23 12:31:22 -0400

    Spin out report details to separate class

--- KenticoInspector.Core/Models/ReportMetadata.cs
+++ KenticoInspector.Core/Models/ReportMetadata.cs
@@ -1,21 +1,9 @@
-﻿using Newtonsoft.Json;
-
-namespace KenticoInspector.Core.Models
+﻿namespace KenticoInspector.Core.Models
 {
     public class ReportMetadata<T> where T: new()
     {
-        public string Name { get; set; }
+        public ReportDetails Details { get; set; }
 
-        public Descriptions Descriptions { get; set; }
-
-        [JsonIgnore]
         public T Terms { get; set; }
-    }
-
-    public class Descriptions
-    {
-        public string Short { get; set; }
-
-        public string Long { get; set; }
     }
 }
