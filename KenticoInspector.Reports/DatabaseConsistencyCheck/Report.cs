215ba85dac80593dd684da41c33037968bc5e29b
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-23 12:43:35 -0400

    Update database consistency analysis report to use abstract and new metadata implementation

--- KenticoInspector.Reports/DatabaseConsistencyCheck/Report.cs
+++ KenticoInspector.Reports/DatabaseConsistencyCheck/Report.cs
@@ -1,44 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-
-using KenticoInspector.Core;
+﻿using KenticoInspector.Core;
 using KenticoInspector.Core.Constants;
+using KenticoInspector.Core.Helpers;
 using KenticoInspector.Core.Models;
 using KenticoInspector.Core.Services.Interfaces;
 using KenticoInspector.Reports.DatabaseConsistencyCheck.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
 
 namespace KenticoInspector.Reports.DatabaseConsistencyCheck
 {
-    public class Report : IReport, IWithMetadata<Labels>
+    public class Report : AbstractReport<Terms>
     {
         private readonly IDatabaseService databaseService;
-        private readonly ILabelService labelService;
 
-        public Report(IDatabaseService databaseService, ILabelService labelService)
+        public Report(IDatabaseService databaseService, IReportMetadataService reportMetadataService) : base(reportMetadataService)
         {
             this.databaseService = databaseService;
-            this.labelService = labelService;
         }
 
-        public string Codename => nameof(DatabaseConsistencyCheck);
+        public override IList<Version> CompatibleVersions => VersionHelper.GetVersionList("10", "11", "12");
 
-        public IList<Version> CompatibleVersions => new List<Version>
-        {
-            new Version(10, 0),
-            new Version(11, 0),
-            new Version(12, 0)
-        };
-
-        public IList<Version> IncompatibleVersions => new List<Version>();
-
-        public IList<string> Tags => new List<string> {
+        public override IList<string> Tags => new List<string> {
             ReportTags.Health
         };
 
-        public Metadata<Labels> Metadata => labelService.GetMetadata<Labels>(Codename);
-
-        public ReportResults GetResults()
+        public override ReportResults GetResults()
         {
 #pragma warning disable 0618 // This is a special exemption as the results of CheckDB are unknown
             var checkDbResults = databaseService.ExecuteSqlFromFileAsDataTable(Scripts.GetCheckDbResults);
@@ -57,7 +44,7 @@ namespace KenticoInspector.Reports.DatabaseConsistencyCheck
                 {
                     Type = ReportResultsType.Table,
                     Status = ReportResultsStatus.Error,
-                    Summary = Metadata.Labels.CheckResultsTableForAnyIssues,
+                    Summary = Metadata.Terms.CheckResultsTableForAnyIssues,
                     Data = checkDbResults
                 };
             }
@@ -67,7 +54,7 @@ namespace KenticoInspector.Reports.DatabaseConsistencyCheck
                 {
                     Type = ReportResultsType.String,
                     Status = ReportResultsStatus.Good,
-                    Summary = Metadata.Labels.NoIssuesFound,
+                    Summary = Metadata.Terms.NoIssuesFound,
                     Data = string.Empty
                 };
             }
