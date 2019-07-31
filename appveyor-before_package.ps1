f49106a6acd77be4a0dd9fafc5dbec55a01776e7
Christopher Jennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-07-31 10:55:01 -0400

    Apply proper scripts for creating artifacts

--- appveyor-before_package.ps1
+++ appveyor-before_package.ps1
@@ -1,26 +1,17 @@
 Write-Output "---Creating Artifacts---"
 
 # Build web application
-
-#Set-Location .\KenticoInspector.WebApplication
-
-#dotnet publish KenticoInspector.WebApplication.csproj /p:PublishDir=..\publish -c Release -r win-x64 --self-contained true
-dotnet publish .\MyConsoleApp\MyConsoleApp.csproj /p:PublishDir=..\publish -c Release -r win-x64 --self-contained true
+Set-Location .\KenticoInspector.WebApplication
+dotnet publish KenticoInspector.WebApplication.csproj /p:PublishDir=..\publish -c Release -r win-x64 --self-contained true
 
 # Copy compiled front-end to publish folder
-
-#Set-Location .\ClientApp
-
+Set-Location .\ClientApp
 mkdir ".\publish\dist"
-
 Copy-Item ".\my-vue-app\dist\*" -Recurse -Destination ".\publish\dist\"
 
 # Go back to root
-
-#Set-Location ..\..\
+Set-Location ..\..\
 
 # Create archive & push to Appveyor
-
 7z a appveyortest-$Env:SEMVER_VERSION.zip .\publish\*
-
 appveyor PushArtifact appveyortest-$Env:SEMVER_VERSION.zip
