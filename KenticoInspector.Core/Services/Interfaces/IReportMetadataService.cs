a52587410196fe6c3b5e2d7737131d7501328657
Yuriy Sountsov <yuriys@kentico.com>
2019-08-07 11:32:02 -0400

    Add support for recursively merging current and default culture yaml

--- KenticoInspector.Core/Services/Interfaces/IReportMetadataService.cs
+++ KenticoInspector.Core/Services/Interfaces/IReportMetadataService.cs
@@ -4,8 +4,12 @@ namespace KenticoInspector.Core.Services.Interfaces
 {
     public interface IReportMetadataService : IService
     {
+        string DefaultCultureName { get; }
+
         string CurrentCultureName { get; }
 
         ReportMetadata<T> GetReportMetadata<T>(string reportCodename) where T : new();
+
+        T DeserializeYaml<T>(string path, bool ignoreUnmatchedProperties = false);
     }
 }
