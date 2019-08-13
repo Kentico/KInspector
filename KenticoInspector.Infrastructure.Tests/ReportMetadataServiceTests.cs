00a91e3daddeb8121dd9475ff9c5c71d17ddd735
Yuriy Sountsov <yuriys@kentico.com>
2019-08-13 17:38:10 -0400

    Add tests for matched and unmatched YAML

---
+++ KenticoInspector.Infrastructure.Tests/ReportMetadataServiceTests.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+using KenticoInspector.Core.Helpers;
+using KenticoInspector.Core.Models;
+using KenticoInspector.Core.Services.Interfaces;
+
+using NUnit.Framework;
+
+namespace KenticoInspector.Infrastructure.Tests
+{
+    [TestFixture]
+    public class ReportMetadataServiceTests
+    {
+        private readonly IReportMetadataService reportMedatadataService;
+
+        public class TestTerms
+        {
+            public Term SingleTerm { get; set; }
+        }
+
+        public ReportMetadataServiceTests()
+        {
+            reportMedatadataService = new ReportMetadataService();
+        }
+
+        [TestCaseSource(typeof(YamlTestCases), nameof(YamlTestCases.YamlMatchesModel))]
+        public void Should_Resolve_When_YamlMatchesModel(
+            string cultureName,
+            string yamlPath,
+            ReportMetadata<TestTerms> resolvedMetadata)
+        {
+            // Arrange
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
+
+            // Act
+            var metadata = reportMedatadataService.GetReportMetadata<TestTerms>(yamlPath);
+
+            // Assert
+            Assert.That(metadata.Details.Name, Is.EqualTo(resolvedMetadata.Details.Name));
+            Assert.That(metadata.Details.ShortDescription, Is.EqualTo(resolvedMetadata.Details.ShortDescription));
+            Assert.That(metadata.Details.LongDescription, Is.EqualTo(resolvedMetadata.Details.LongDescription));
+
+            Assert.That(metadata.Terms.SingleTerm.ToString(), Is.EqualTo(resolvedMetadata.Terms.SingleTerm.ToString()));
+        }
+
+        [TestCase("en-US", "TestData\\YamlDoesNotMatch")]
+        public void Should_Throw_When_YamlDoesNotMatchModel(
+            string cultureName,
+            string yamlPath)
+        {
+            // Arrange
+            Thread.CurrentThread.CurrentCulture = new CultureInfo(cultureName);
+
+            // Act
+            ReportMetadata<TestTerms> getReportMetadata(string path) => reportMedatadataService
+                    .GetReportMetadata<TestTerms>(path);
+
+            // Assert
+            Assert.That(() => getReportMetadata(yamlPath), Throws.Exception);
+        }
+
+        public class YamlTestCases
+        {
+            public static IEnumerable<TestCaseData> YamlMatchesModel
+            {
+                get
+                {
+                    yield return GetTestCaseData("en-US", "TestData\\YamlMatches", new ReportMetadata<TestTerms>()
+                    {
+                        Details = new ReportDetails()
+                        {
+                            Name = "Details name",
+                            ShortDescription = "Details shortDescription",
+                            LongDescription = "Details longDescription\n"
+                        },
+                        Terms = new TestTerms()
+                        {
+                            SingleTerm = "Term value"
+                        }
+                    });
+                    yield return GetTestCaseData("en-GB", "TestData\\YamlMatches", new ReportMetadata<TestTerms>()
+                    {
+                        Details = new ReportDetails()
+                        {
+                            Name = "British details name",
+                            ShortDescription = "Details shortDescription",
+                            LongDescription = "Details longDescription\n"
+                        },
+                        Terms = new TestTerms()
+                        {
+                            SingleTerm = "British term value"
+                        }
+                    });
+                }
+            }
+
+            private static TestCaseData GetTestCaseData(
+                string cultureCode,
+                string yamlPath,
+                ReportMetadata<TestTerms> resolvedMetadata)
+            {
+                return new TestCaseData(cultureCode, yamlPath, resolvedMetadata)
+                    .SetName($"Metadata in culture \"{cultureCode}\" resolves.");
+            }
+        }
+    }
+}
