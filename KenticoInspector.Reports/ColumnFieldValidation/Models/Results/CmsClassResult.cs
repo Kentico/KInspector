9aad0eab94df7797130045f1920e764a37766ae0
Yuriy Sountsov <yuriys@kentico.com>
2019-08-19 14:53:37 -0400

    Initial commit

---
+++ KenticoInspector.Reports/ColumnFieldValidation/Models/Results/CmsClassResult.cs
@@ -0,0 +1,15 @@
+﻿namespace KenticoInspector.Reports.ColumnFieldValidation.Models.Results
+{
+    public class CmsClassResult
+    {
+        public int ClassID { get; set; }
+
+        public string ClassName { get; set; }
+
+        public string ClassDisplayName { get; set; }
+
+        public string ClassTableName { get; set; }
+
+        public string ClassFieldsNotInTable { get; set; }
+    }
+}
