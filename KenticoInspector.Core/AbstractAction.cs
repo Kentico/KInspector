8511935c01001403d0c7376ab477adedfb8c7c20
christopherjennings <1398135+ChristopherJennings@users.noreply.github.com>
2019-08-31 15:25:54 -0400

    Add action abstract, result, and interface

---
+++ KenticoInspector.Core/AbstractAction.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using KenticoInspector.Core.Models;
+using KenticoInspector.Core.Modules;
+using KenticoInspector.Core.Services.Interfaces;
+
+namespace KenticoInspector.Core
+{
+    public abstract class AbstractAction<T> : AbstractModule<T>, IAction where T : new()
+    {
+        public AbstractAction(IModuleMetadataService moduleMetadataService) : base(moduleMetadataService)
+        {
+        }
+
+        public abstract ActionResult Execute<TOptions>(TOptions ActionOptions) where TOptions : new();
+    }
+}
---
+++ KenticoInspector.Core/Models/ActionResult.cs
@@ -0,0 +1,17 @@
+﻿using KenticoInspector.Core.Constants;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KenticoInspector.Core.Models
+{
+    public class ActionResult
+    {
+        [JsonConverter(typeof(StringEnumConverter))]
+        public ResultsStatus Status { get; set; }
+
+        public string Summary { get; set; }
+    }
+}
