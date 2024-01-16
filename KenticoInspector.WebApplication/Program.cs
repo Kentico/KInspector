d790bff57f1633849b9062dadef1cbf1bd0a2e52
Josef Dvorak <JosefDvorak@users.noreply.github.com>
2024-01-17 08:23:15 +1100

    Update Program.cs

--- KenticoInspector.WebApplication/Program.cs
+++ KenticoInspector.WebApplication/Program.cs
@@ -5,7 +5,7 @@ namespace KenticoInspector.WebApplication
 {
     //Testing appveyor commit 2
 
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
